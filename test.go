4e1ad8f8d80fcb011ae8c69da2a719cc7cf66be8
karanghorpade <karan.ghorpade@sjsu.edu>
2020-04-05 11:13:15 -0700

    HR problems third commit

--- test.go
+++ test.go
@@ -3,11 +3,27 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
+ 	var n,sl,valley int
+	var s string
+ 	fmt.Scan(&n)
 
+ 	fmt.Scan(&s)
+
+ 	for _,v:=range s{
+		if(v=='U'){
+			sl++
+		}else{
+			sl--
+		}
+
+		if(sl==0 && v=='U'){
+			valley++
+		}
+	}
+	fmt.Println(valley)
 
 }
 
